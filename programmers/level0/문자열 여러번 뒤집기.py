997a627f8d30c0c99041d972c4cf69b8cf273c32
kuk329 <kuk329@cau.ac.kr>
2023-06-16 01:37:50 +0900

    6/15 level0

--- programmers/level0/문자열 여러번 뒤집기.py
+++ programmers/level0/문자열 여러번 뒤집기.py
@@ -6,24 +6,35 @@
 # 원래 문자열 변경이 아닌 바뀐 문자열을 반환
 
 
+# def solution(my_string, queries):
+#     answer = ''
+#     for s,e in queries:
+#         before = my_string[s:e+1]
+#         after = before[::-1]
+#         j=0
+#         for i in range(s,e+1):
+#             my_string[i]=after[j]
+#             j+=1
+  
+
+#     return my_string
+
 def solution(my_string, queries):
-    answer = ''
+   
     for s,e in queries:
         before = my_string[s:e+1]
         after = before[::-1]
-        j=0
-        for i in range(s,e+1):
-            my_string[i]=after[j]
-            j+=1
-
-
-        
+        my_string=my_string[:s]+after+my_string[e+1:]
 
     return my_string
 
-
-
+    
 my_string = "rermgorpsam"
 queries=[[2, 3], [0, 7], [5, 9], [6, 10]]
+
+print(solution(my_string,queries))
+
+my_string = "abab"
+queries=[[2, 3]]
 
 print(solution(my_string,queries))
--- programmers/level0/주사위 게임 3.py
+++ programmers/level0/주사위 게임 3.py
@@ -1,6 +1,37 @@
-
-
-
 def solution(a, b, c, d):
     answer = 0
+    if a==b==c==d:
+        answer=a*1111
+    elif a==b==c:
+        answer=(10*a+d)*(10*a+d)
+    elif b==c==d:
+        answer=(10*b+a)*(10*b+a)
+    elif a==b==d:
+        answer=(10*a+c)*(10*a+c)
+    elif a==c==d:
+        answer=(10*a+b)*(10*a+b)
+    elif a==b and c==d:
+        answer=(a+c)*abs(a-c)
+    elif a==c and b==d:
+        answer=(a+b)*abs(a-b)
+    elif a==d and b==c:
+        answer=(a+b)*abs(a-b)
+    elif a==b and c!=d:
+        answer=(c*d)
+    elif a==c and b!=d:
+        answer=b*d
+    elif a==d and b!=c:
+        answer=b*c  
+    elif b==c and a!=d:
+        answer=a*d
+    elif b==d and a!=c:
+        answer=a*c
+    elif c==d and a!=b:
+        answer=a*b
+    elif a!=b!=c!=d:
+        answer = min(a,b,c,d)
+        
     return answer
+    
+
+
