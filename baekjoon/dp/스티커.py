3b0f5eeefc48902ddcbb715140022ea1f2cb8754
kuk329 <kuk329@cau.ac.kr>
2023-08-04 21:57:48 +0900

    스티커-수정

--- baekjoon/dp/스티커.py
+++ baekjoon/dp/스티커.py
@@ -3,7 +3,7 @@
 t = int(input()) # 테스트 케이스 입력받음.
 
 for i in range(t):
-    n = int(input()) # 스티커 수 입력받음.
+    n = int(input()) # 스티커 수 입력받음(한줄)
 
     a = [[] for i in range(2)]
 
@@ -12,18 +12,26 @@ for i in range(t):
 
     d=[[0]*n for i in range(2)]
     # 행 열
-    d[0][0]=a[0][0]
-    d[1][0]=a[1][0]
-    d[0][1]=a[1][0]+a[0][1]
-    d[1][1]=a[0][0]+a[1][1]
+    if n==1:
+        print(max(a[0][0],a[1][0]))
+    else:
+        d[0][0]=a[0][0]
+        d[1][0]=a[1][0]
+        d[0][1]=a[1][0]+a[0][1]
+        d[1][1]=a[0][0]+a[1][1]
+
+        for i in range(2,n):
+            d[0][i]=a[0][i]+max(d[1][i-1],d[1][i-2])
+            d[1][i]=a[1][i]+max(d[0][i-1],d[0][i-2])
+            
+        print(max(d[0][n-1],d[1][n-1]))
+    
+
+
 
     
 
-    for i in range(2,n):
-        d[0][i]=a[0][i]+max(d[1][i-1],d[1][i-2])
-        d[1][i]=a[1][i]+max(d[0][i-1],d[0][i-2])
-
-    print(max(d[0][n-1],d[1][n-1]))
+    
 
 
 
