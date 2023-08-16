0d9aa99596d6c35a750bed20de8e8b7dda90dcad
kuk329 <kuk329@cau.ac.kr>
2023-08-16 13:33:11 +0900

    level2-영어 끝말잇기

--- programmers/level2/영어 끝말잇기.py
+++ programmers/level2/영어 끝말잇기.py
@@ -1,29 +1,28 @@
 
-n = int(input())
+def solution(n, words):
+    answer = []
+    tmp=[]
+    person_num=0
+    repeat=0
+    answer=[0,0]
+    for i in range(len(words)):
+        if words[i] in tmp: # 탈락  
+            person_num=i%n+1
+            repeat = i//n+1
+            break
+        elif tmp and tmp[-1][-1]!=words[i][0]:
+            
+            person_num=i%n+1
+            repeat = i//n+1
+            break
+        else:
+            tmp.append(words[i]) # 통과
 
-coordinate = list(map(int,input().split()))
-
-d=dict()
-for i in range(n):
-    d[coordinate[i]]=i # dict 압축전 값: index // index -> 압축 값으로 변경
-
-_coordinate = sorted(coordinate)
-# -10 -9 2 4 4 
-#  0  1  2 3 3 -> 앞 뒤값과 중복만 아니면 0부터 하나씩 증가
-
-press=[0]
-cnt=0
-for i in range(1,n):
-    if press[-1]!=_coordinate[i]:
-        cnt+=1
-        press.append(cnt)
-        print(f'press : {press[-1]}')
-        print(f'coord: {_coordinate[i]}')
-    else:
-        press.append(cnt)
-  
-
-
-
-print(press)
+    answer=[person_num,repeat]
  
+
+    return answer
+
+
+
+print(solution(3,["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
