de18d1d7eb0a0206aa8cade1e86aeed60562d98c
kuk329 <kuk329@cau.ac.kr>
2023-09-12 12:22:37 +0900

    백준-미로 탐색

---
+++ baekjoon/graph/미로 탐색.py
@@ -0,0 +1,38 @@
+# 2178번
+
+from collections import deque
+from typing import ValuesView
+
+n,m = map(int,input().split())
+
+a=[]
+d=[[0]*m for _ in range(n)]
+
+for _ in range(n):
+    tmp=[int(i) for i in list(input())]
+    a.append(tmp)
+
+dx=[-1,1,0,0]
+dy=[0,0,1,-1]
+
+def bfs(i,j):
+    q=deque()
+    q.append((i,j))
+    d[i][j]=1
+    while q:
+        x,y = q.popleft()
+        for k in range(4):
+            nx=x+dx[k]
+            ny=y+dy[k]
+            if nx>=0 and nx<n and ny>=0 and ny<m:
+                if d[nx][ny]==0 and a[nx][ny]==1:
+                    d[nx][ny]=d[x][y]+1
+                    q.append((nx,ny))
+
+
+
+bfs(0,0)
+
+print(d[n-1][m-1])
+
+
