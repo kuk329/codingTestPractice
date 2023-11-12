4abe7bb479921821d4c9a3a55f204859d597348b
kuk329 <kuk329@cau.ac.kr>
2023-11-13 01:39:42 +0900

    swea-lv2. 백만 장자 프로젝트

---
+++ swea/lv2/백만 장자 프로젝트.py
@@ -0,0 +1,18 @@
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1): # 
+    n = int(input())
+    arr = list(map(int,input().split()))
+    result = 0
+    max_num=0
+    for val in arr[::-1]:
+        if max_num<val:
+            max_num=val
+        else:
+            result+= (max_num-val)
+    
+            
+        
+    print(f'#{test_case} {result}')
+    
